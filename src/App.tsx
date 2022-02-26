609d4d2bca2b617d309c7a4a0c11de0180a047af
VarnerDamascenoJr <varnerdjr@gmail.com>
2022-02-26 16:11:16 -0300

    The initial setup of the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {Header} from "./components/Header/index";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Teste text</h1>
+    </div>
+  );
+}
+
+export default App;
