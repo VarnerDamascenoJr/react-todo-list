8e90d83ee9d8ccbc3adf62d7f22ae71a8620424f
VarnerDamascenoJr <varnerdjr@gmail.com>
2023-03-24 09:52:46 -0300

    CRUD functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,76 @@
-import React from "react";
+import React, { useState} from "react";
 
 import {Header} from "./components/Header/index";
 import "./App.css";
+import { Tasks } from "./components/Tasks";
+import { TasksInterface } from "./interface/interface";
+import { CreateTaskForm, IFormInput } from "./components/CreateTaskForm";
+
+
+
 
 function App() {
+//{id:number, name:string, date: string, isCompleted: boolean}[]
+  const [tasks, setTasks] = useState<TasksInterface []>([
+    {
+        id: 1, 
+        name: "Task 1",
+        date: "10th september",
+        isCompleted:false
+    },
+    {
+        id: 2, 
+        name: "Task 2",
+        date: "10th september",
+        isCompleted:false
+    },
+    {
+        id: 3, 
+        name: "Task 3",
+        date: "10th september",
+        isCompleted:false
+    },
+    {
+        id: 4, 
+        name: "Task 4",
+        date: "10th september",
+        isCompleted:false
+    },
+    {
+        id: 5, 
+        name: "Task 5",
+        date: "10th september",
+        isCompleted:false
+    },
+
+]);
+
+const createTask = (data:IFormInput):void=>{
+
+  const id = Math.random()*11000+ 12;
+  console.log({...data, id});
+};
+
+const deleteTask = (id:TasksInterface["id"]):void=>{
+  //console.log(id);
+  setTasks(
+    tasks.filter((task)=> task.id !==id)
+  );
+};
+
+const toggletask = (id:TasksInterface["id"]):void=>{
+  setTasks(
+    tasks.map((task)=>task.id === id ? {...task, isCompleted:!task.isCompleted} : task)
+  );
+};
+
   return (
     <div className="App">
       <Header />
+
       <h1>Teste text</h1>
+      <CreateTaskForm onCreate={createTask} />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggletask}/>
     </div>
   );
 }
