147e39d109d32affea59ab990f4b854ba810d451
VarnerDamascenoJr <varnerdjr@gmail.com>
2023-03-30 18:31:26 -0300

    Adding the button and the functions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,11 @@ export const Header = ({ showForm ,onShow}:HeaderType) =>{
     return(
         <nav className="container space">
             <h1 className="title">Todo List </h1>
-            <ButtonDefault fill="filled" size="small" text={showForm ? "Close":"Show Form"}  onClick={()=>onShow(!showForm)} type="button"/>
+            <ButtonDefault 
+                fill="filled" 
+                size="small" 
+                text={showForm ? "Close":"Show Form"}  
+                onClick={()=>onShow(!showForm)} type="button"/>
         </nav>
     );
 };
